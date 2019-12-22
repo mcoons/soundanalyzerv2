38a9106e11844ba885bf6680ace6dfbe789ae582
mcoons <mcoons67@gmail.com>
2019-12-21 18:41:35 -0700

    cleaned code

--- app/index.js
+++ app/index.js
@@ -4,6 +4,17 @@ window.onload = function() {
   
     var file = document.getElementById("thefile");
     var audio = document.getElementById("audio");
+    var canvas2D = document.getElementById("canvas2D");
+    var ctx2D = canvas2D.getContext("2d");
+    var title = document.getElementById("title");
+
+    var audioCtx = new AudioContext();
+    var audioSrc = audioCtx.createMediaElementSource(audio);
+    var analyser = audioCtx.createAnalyser();
+    analyser.fftSize = 256;
+    var bufferLength = analyser.frequencyBinCount;
+    analyser.connect(audioCtx.destination);
+    audioSrc.connect(analyser);
 
     $('.new_Btn').bind("click" , function () {
         $('#thefile').click();
@@ -11,57 +22,42 @@ window.onload = function() {
     
     file.onchange = function() {
       var files = this.files;
+
+      title.innerHTML = files[0].name;
+
       audio.src = URL.createObjectURL(files[0]);
       audio.load();
       audio.play();
-      var context = new AudioContext();
-      var src = context.createMediaElementSource(audio);
-      var analyser = context.createAnalyser();
 
-  
-      var canvas = document.getElementById("canvas2D");
+      canvas2D.style.width = canvas2D.width;
+      canvas2D.style.height = canvas2D.height;
 
-      var ctx = canvas.getContext("2d");
-
-      canvas.style.width = canvas.width;
-      canvas.style.height = canvas.height;
-
-      src.connect(analyser);
-      analyser.connect(context.destination);
-  
-      analyser.fftSize = 128;
-  
-      var bufferLength = analyser.frequencyBinCount;
-      console.log(bufferLength);
   
       var dataArray = new Uint8Array(bufferLength);
   
-      var WIDTH = canvas.width;
-      var HEIGHT = canvas.height;
+      var WIDTH = canvas2D.width;
+      var HEIGHT = canvas2D.height;
   
-      var dataLength = bufferLength-15; 
-      var barWidth = (WIDTH / dataLength) -2 ;
+      var dataLength = bufferLength-32; 
+      var barWidth = (WIDTH / dataLength) -3;
       var barHeight;
       var x = 0;
   
       var dpi = window.devicePixelRatio;
-
-      document.getElementById("title").innerHTML = files[0].name;
-
       
       function fix_dpi() {
       //create a style object that returns width and height
         let style = {
           height() {
-            return +getComputedStyle(canvas).getPropertyValue('height').slice(0,-2);
+            return +getComputedStyle(canvas2D).getPropertyValue('height').slice(0,-2);
           },
           width() {
-            return +getComputedStyle(canvas).getPropertyValue('width').slice(0,-2);
+            return +getComputedStyle(canvas2D).getPropertyValue('width').slice(0,-2);
           }
         }
       //set the correct attributes for a crystal clear image!
-        canvas.setAttribute('width', style.width() * dpi);
-        canvas.setAttribute('height', style.height() * dpi);
+        canvas2D.setAttribute('width', style.width() * dpi);
+        canvas2D.setAttribute('height', style.height() * dpi);
       }
 
       function render2DFrame() {
@@ -73,29 +69,39 @@ window.onload = function() {
   
         fix_dpi();
 
-        WIDTH = canvas.width;
-        HEIGHT = canvas.height;
+        WIDTH = canvas2D.width;
+        HEIGHT = canvas2D.height;
     
-        barWidth = (WIDTH / dataLength) -2;
+        barWidth = (WIDTH / dataLength) -3;
 
-        ctx.clearRect(0, 0, WIDTH, HEIGHT);
+        ctx2D.clearRect(0, 0, WIDTH, HEIGHT);
+
+        let textColor;
   
         for (var i = 0; i < bufferLength-10; i++) {
-          barHeight = dataArray[i]*1.5;
+          barHeight = dataArray[i]*1+3;
           
           var r = barHeight + (.22 * (i/dataLength));
           var g = 250 * (i/dataLength);
           var b = 250;
   
-          ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-          ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+          if (i==20) textColor = "rgb(" + r + "," + g + "," + b + ")";
+
+          ctx2D.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+          ctx2D.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+
   
-          x += barWidth + 2;
+          x += barWidth + 3;
         }
+        title.style.color = textColor;
+
       }
   
-      audio.play();
+    //   audio.play();
       render2DFrame();
+
+
+
     };
   };
 
